b1cf33428987ece5ce98b7918caeefc1bf197b33
Gustavo L <95352419+G-code7@users.noreply.github.com>
2024-11-23 11:43:50 -0400

    Update infoTourPlans.js

--- src/front/js/component/infoTourPlans.js
+++ src/front/js/component/infoTourPlans.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useContext } from "react";
 import { Context } from "../store/appContext";
 import { useParams } from "react-router-dom";
 import { Modal } from "react-bootstrap";
-import "/workspaces/VenTu-project/src/front/styles/InfoTourPlans.css";
+// import "/workspaces/VenTu-project/src/front/styles/InfoTourPlans.css";
 
 const InfoTourPlans = () => {
     const { id } = useParams();
